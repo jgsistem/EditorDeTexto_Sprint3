be1d8c469e07a255ccece411c1671244ebef78cc
jgsistem <jgsistem@hotmail.com>
2018-06-20 03:01:31 -0500

    Subir

--- EditorDeTexto_SW/Buscar.cs
+++ EditorDeTexto_SW/Buscar.cs
@@ -10,20 +10,32 @@ using EditorDeTexto_SW;
 
 namespace EditorDeTexto_SW
 {
-     
+
     public partial class Buscar : Form
     {
         RichTextBox txtEditex;
-        bool RedTipo;
+        static Color colorOriginal;
+        static Color backColorOriginal;
+        static String textoOriginalBusqueda;
+        static int totalCoincidencias;
+        static int coincidenciaActual;
+
         public Buscar(RichTextBox Texto)
-        {           
+        {
             InitializeComponent();
-            txtEditex = Texto;            
+            txtEditex = Texto;
+            colorOriginal = txtEditex.ForeColor;
+            backColorOriginal = txtEditex.BackColor;
+            textoOriginalBusqueda = "";
         }
 
-        
+
         private void btnCerrar_Click(object sender, EventArgs e)
         {
+            txtEditex.Select(0, txtEditex.Text.Length);
+            txtEditex.SelectionColor = colorOriginal;
+            txtEditex.SelectionBackColor = backColorOriginal;
+            txtEditex.SelectionLength = 0;
             this.Close();
         }
 
@@ -31,18 +43,27 @@ namespace EditorDeTexto_SW
         {
             try
             {
-               //VerifiBusque(txtEditex, txtBuscar.Text, Color.Black);
-               Busqueda(txtEditex, txtBuscar.Text, Color.Red);
+                if (txtBuscar.Text == "")
+                {
+                    MessageBox.Show("Por favor ingresar el texto a buscar");
+                    txtBuscar.Focus();
+                }
+                else
+                {
+                    //VerifiBusque(txtEditex, txtBuscar.Text, Color.Black);
+                    Busqueda(txtEditex, txtBuscar.Text, Color.Red);
+                }
             }
             catch (Exception)
             {
 
                 throw;
             }
-            finally {
-                
+            finally
+            {
+
             }
-                      
+
         }
 
 
@@ -64,8 +85,8 @@ namespace EditorDeTexto_SW
 
             TextoTotal.SelectionStart = s_start;
             TextoTotal.SelectionLength = 0;
-            TextoTotal.SelectionColor = Color.Black;   
-        
+            TextoTotal.SelectionColor = Color.Black;
+
         }
 
         static void Busqueda(RichTextBox TextoTotal, String ListaTexto, Color color)
@@ -73,28 +94,67 @@ namespace EditorDeTexto_SW
             if (ListaTexto == "")
             {
                 return;
-            }           
-            int s_start = TextoTotal.SelectionStart, startIndex = 0, index;   
+            }
+            if (textoOriginalBusqueda == "" || textoOriginalBusqueda.CompareTo(ListaTexto) != 0)
+            {
+                textoOriginalBusqueda = ListaTexto;
+                coincidenciaActual = 1;
+            }
+            else
+            {
+                coincidenciaActual++;
+            }
+           
+            totalCoincidencias = 0;
+            TextoTotal.Select(0, TextoTotal.Text.Length);
+            TextoTotal.SelectionColor = colorOriginal;
+            TextoTotal.SelectionBackColor = backColorOriginal;
+            TextoTotal.SelectionLength = 0;           
 
+            int s_start = TextoTotal.SelectionStart, startIndex = 0, index;
+            int contador = 0;
             while ((index = TextoTotal.Text.IndexOf(ListaTexto, startIndex)) != -1)
             {
                 TextoTotal.Select(index, ListaTexto.Length);
                 TextoTotal.SelectionColor = color;
                 startIndex = index + ListaTexto.Length;
-               
-            }        
 
+                totalCoincidencias++;
+                contador++;
+                if (contador == coincidenciaActual)
+                {
+                    TextoTotal.ScrollToCaret();
+                    TextoTotal.SelectionBackColor = Color.Cyan;
+                }
+            }
+            if (coincidenciaActual == totalCoincidencias)
+            {
+                coincidenciaActual = 0;
+            }
+            if (totalCoincidencias == 0)
+            {
+                coincidenciaActual = 0;
+                MessageBox.Show("No se encontraron resultados");
+            }
+            //textoOriginalBusqueda
             TextoTotal.SelectionStart = s_start;
             TextoTotal.SelectionLength = 0;
-            TextoTotal.SelectionColor = Color.Black;   
+            TextoTotal.SelectionColor = Color.Black;
 
         }
 
         private void Buscar_Load(object sender, EventArgs e)
         {
-           // Buscar.FormBorderStyle = FormBorderStyle.FixedSingle;
-           
-        } 
-       
+            // Buscar.FormBorderStyle = FormBorderStyle.FixedSingle;
+
+        }
+
+        private void Buscar_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            txtEditex.Select(0, txtEditex.Text.Length);
+            txtEditex.SelectionColor = colorOriginal;
+            txtEditex.SelectionBackColor = backColorOriginal;
+            txtEditex.SelectionLength = 0;
+        }
     }
 }
